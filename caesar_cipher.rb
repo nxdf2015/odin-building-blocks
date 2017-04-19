ec902d5e328a2e9cd08304a81dd1584018b0354a
info2016 <niquet_xavier2002@yahoo.fr>
2017-04-20 00:45:21 +0200

    two versions

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -45,20 +45,4 @@ end
 
 #print caesar_cipher("What a string!",5) #Bmfy f xywnsl!
 
-##other version
-
-def caesar_cipher2(text,shift)
-   letters = ('a'..'z').to_a
-   all_letters = letters +  letters.map { |l| l.upcase }
-   code_letters= letters.rotate(shift)
-   code_letters += code_letters.map  {|l| l.upcase }
-   text.tr(all_letters.join(""),code_letters.join(""))
-end
-
-
-
-
-
-
-
-print caesar_cipher2("What a string!",5) #Bmfy f xywnsl!
+ 
