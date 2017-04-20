f1ca3cf60f521adeed127c1dbf18f3af5bd6e017
info2016 <niquet_xavier2002@yahoo.fr>
2017-04-20 09:20:55 +0200

    add stock-picker

---
+++ stock-picker/stock-picker.rb
@@ -0,0 +1,21 @@
+
+
+def stock_picker_value(prices  )
+  if prices.length == 2
+    return prices
+  end
+  price_min = prices.shift
+  price_max = prices.max
+  value = stock_picker_value(prices)
+  return price_max - price_min < (value[1] - value[0]) ? value : [price_min,price_max]
+end
+
+def stock_picker(prices)
+  price_min,price_max = stock_picker_value(prices.clone)
+  [ prices.index(price_min) , prices.index(price_max)]
+end
+
+p stock_picker([1,2,3])
+
+p stock_picker([4,1,2,3])
+p stock_picker([17,3,6,9,15,8,6,1,10])
